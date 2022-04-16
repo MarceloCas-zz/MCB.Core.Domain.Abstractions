966943bc637f1fff69051457cfdadd9cc77407e1
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-16 20:14:51 -0300

    fix: rename properties

--- src/MCB.Core.Domain.Abstractions/DomainEvents/IDomainEvent.cs
+++ src/MCB.Core.Domain.Abstractions/DomainEvents/IDomainEvent.cs
@@ -5,8 +5,8 @@
         Guid EventId { get; }
         Guid CorrelationId { get; }
         DateTimeOffset TimeStamp { get; }
-        string MessageType { get; }
-        string MessageSchema { get; }
+        string EventType { get; }
+        string EventSchema { get; }
         Guid TenantId { get; }
         string ExecutionUser { get; }
         string SourcePlatform { get; }
