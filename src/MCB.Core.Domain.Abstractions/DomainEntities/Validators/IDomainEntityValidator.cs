8f07c9988eac32a0cbfae57a285b729f12bc7694
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-17 01:40:52 -0300

    feat: add IDomainEntityValidator

---
+++ src/MCB.Core.Domain.Abstractions/DomainEntities/Validators/IDomainEntityValidator.cs
@@ -0,0 +1,11 @@
+﻿using MCB.Core.Domain.Entities.Abstractions;
+using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Validator;
+
+namespace MCB.Core.Domain.Abstractions.DomainEntities.Validators
+{
+    public interface IDomainEntityValidator<TDomainEntity>
+        : IValidator<TDomainEntity>
+        where TDomainEntity : IDomainEntity
+    {
+    }
+}
