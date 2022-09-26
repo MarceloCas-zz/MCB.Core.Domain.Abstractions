842052606be0e0db10c6e854e3c232674b87c4a2
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-09-26 00:05:56 -0300

    refact: add project files in sln

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This project is a source code of [MCB.Core.Domain.Abstractions](https://www.nuge
 ## Labels
 
 [![build-and-test](https://github.com/MarceloCas/MCB.Core.Domain.Abstractions/actions/workflows/build-and-test.yml/badge.svg?branch=main)](https://github.com/MarceloCas/MCB.Core.Domain.Abstractions/actions/workflows/build-and-test.yml)
-
+![Nuget](https://img.shields.io/nuget/v/MCB.Core.Domain.Abstractions)
 
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=MarceloCas_MCB.Core.Domain.Abstractions&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=MarceloCas_MCB.Core.Domain.Abstractions)
 [![Coverage](https://sonarcloud.io/api/project_badges/measure?project=MarceloCas_MCB.Core.Domain.Abstractions&metric=coverage)](https://sonarcloud.io/summary/new_code?id=MarceloCas_MCB.Core.Domain.Abstractions)
--- src/MCB.Core.Domain.Abstractions/DomainEntities/Specifications/IDomainEntitySpecifications.cs
+++ src/MCB.Core.Domain.Abstractions/DomainEntities/Specifications/IDomainEntitySpecifications.cs
@@ -1,17 +1,9 @@
-﻿using FluentValidation;
-using MCB.Core.Domain.Entities.Abstractions;
+﻿using MCB.Core.Domain.Entities.Abstractions;
 
 namespace MCB.Core.Domain.Abstractions.DomainEntities.Specifications;
 
 public interface IDomainEntitySpecifications<TDomainEntity>
     where TDomainEntity : IDomainEntity
 {
-    void AddIdIsRequiredSpecification(AbstractValidator<TDomainEntity> validator);
-    void AddTenantIdIsRequiredSpecification(AbstractValidator<TDomainEntity> validator);
-    void AddCreationInfoIsRequiredSpecification(AbstractValidator<TDomainEntity> validator);
-    void AddCreationInfoIsValidSpecification(AbstractValidator<TDomainEntity> validator);
-    void AddUpdateInfoIsRequiredSpecification(AbstractValidator<TDomainEntity> validator);
-    void AddUpdateInfoIsValidSpecification(AbstractValidator<TDomainEntity> validator);
-    void AddRegistryVersionIsRequiredSpecification(AbstractValidator<TDomainEntity> validator);
-    void AddRegistryVersionIsValidSpecification(AbstractValidator<TDomainEntity> validator);
+
 }
